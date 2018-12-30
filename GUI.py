e2393739b83d7291fda8a45322f6725a2d3a633b
hubert <hubglo@wp.pl>
2018-12-31 00:04:39 +0100

    gui changed

--- GUI.py
+++ GUI.py
@@ -2,32 +2,41 @@
 
 import tkinter as tk
 from KittelPlotter import *
+import VNA
+import Gaussmeter
+
+odstep = 4
+
 
 class VNA_FMR(tk.Frame):
-   
+
     def __init__(self, parent):
         tk.Frame.__init__(self, parent)
 
-        self.root=parent
+        self.root = parent
 
-        self.buttonContainer=tk.Frame(self)
+        self.buttonContainer = tk.Frame(self)
         self.buttonContainer.pack(side='left')
-    
-        self.button = tk.Button(self.buttonContainer, text="Kittel plotter", 
+
+        self.button = tk.Button(self.buttonContainer, text="Kittel plotter",
                                 command=self.create_window)
-        self.button.pack(side="top", padx=100, pady=100)
+        self.button.pack(side="top", padx=odstep, pady=odstep)  # , padx=100, pady=100)
 
-        self.buttonDane = tk.Button(self.buttonContainer, text="printDane", 
-                                command=self.printDane)
-        self.buttonDane.pack()
+        self.buttonSetup = tk.Button(self.buttonContainer, text='Measurement setup',
+                                     command=self.SetupMeas)
+        self.buttonSetup.pack(pady=odstep, padx=odstep)
 
+        self.buttonDane = tk.Button(self.buttonContainer, text="printDane",
+                                    command=self.printDane)
+        self.buttonDane.pack(pady=odstep, padx=odstep)
 
-        self.graphContainer=tk.Frame(self)
+        self.graphContainer = tk.Frame(self)
         self.graphContainer.pack(side='left')
 
         self.fig = Figure(figsize=(5, 4), dpi=100)
 
-        self.canvas = FigureCanvasTkAgg(self.fig, master=self.graphContainer)  # A tk.DrawingArea.
+        # A tk.DrawingArea.
+        self.canvas = FigureCanvasTkAgg(self.fig, master=self.graphContainer)
         self.graph1 = self.fig.add_subplot(111)
         self.canvas.draw()
         self.canvas.get_tk_widget().pack(side=tk.TOP, fill=tk.BOTH, expand=1)
@@ -35,13 +44,21 @@ class VNA_FMR(tk.Frame):
         toolbar = NavigationToolbar2Tk(self.canvas, self.graphContainer)
         toolbar.update()
         self.canvas.get_tk_widget().pack(side=tk.TOP, fill=tk.BOTH, expand=1)
-        
+
+        # self.vna=VNA.VNA()
+        # self.gm=Gaussmeter.Gaussmeter()
+
+    def SetupMeas(self):
+        # vna.Setup()
+        # gm.Setup()
+        pass
+
     def create_window(self):
         # self.hide()
         self.t = KittelPlotter(self)
 
     def printDane(self):
-        print(self.t.dane)   
+        print(self.t.dane)
 
     def show(self):
         self.root.update()
@@ -49,12 +66,12 @@ class VNA_FMR(tk.Frame):
 
     def hide(self):
         self.root.withdraw()
-  
-        
+
+
 if __name__ == "__main__":
     root = tk.Tk()
     root.wm_title("VNA-FMR")
-    
+
     main = VNA_FMR(root)
     main.pack(side="top", fill="both", expand=True)
     root.mainloop()
